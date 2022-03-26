cc0978a8816238422f84d308f517ea30024a0d06
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-03-26 16:53:21 +0530

    Fixed AB#15
    Task is finished

--- NewRepo/Program.cs
+++ NewRepo/Program.cs
@@ -20,7 +20,7 @@ namespace NewRepo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();//done
                 });
     }
 }
