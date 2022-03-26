673e5801df66e61d6289f3187695158b9b912a35
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-03-26 16:55:52 +0530

    Fixed AB#13

--- NewRepo/Program.cs
+++ NewRepo/Program.cs
@@ -20,7 +20,7 @@ namespace NewRepo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();//done11
+                    webBuilder.UseStartup<Startup>();//done111
                 });
     }
 }
